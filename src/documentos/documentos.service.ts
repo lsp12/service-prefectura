70f4ca5d72779a95c80f7c708de9b037e9465985
lsp12 <jonathankenny852@gmail.com>
2022-03-23 21:19:07 -0500

    solution bugs

--- src/documentos/documentos.service.ts
+++ src/documentos/documentos.service.ts
@@ -1,3 +1,4 @@
+/* eslint-disable prefer-const */
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
@@ -13,6 +14,9 @@ import { PDFDocument } from 'pdf-lib';
 import * as qrcode from 'qrcode';
 
 const libreAsyncConvert = util.promisify(libre.convert);
+
+//hacer un contador que empiece en 001 y que se incremente en 1
+let contador = 1;
 
 @Injectable()
 export class DocumentosService {
@@ -35,67 +39,83 @@ export class DocumentosService {
       tipoDoc: 'DNI',
       cuerpo: 'cuerpo',
     }; */
-    carbone.render('./src/doc/Oficio.docx', data, async function (err, result) {
-      if (err) {
-        return console.log(err);
-      }
-      // write the result
-      fs.writeFileSync('./src/documentos/result.docx', result);
+    let aux = '';
+    if (contador >= 100) {
+      aux = contador.toString();
+    } else if (contador >= 10) {
+      aux = '0' + contador;
+    } else {
+      aux = '00' + contador;
+    }
+    contador++;
+    data.contador = aux;
+    carbone.render(
+      './src/doc/Modelo Oficio.docx',
+      data,
+      async function (err, result) {
+        if (err) {
+          return console.log(err);
+        }
+        // write the result
+        fs.writeFileSync('./src/documentos/result.docx', result);
 
-      const ext = '.pdf';
-      const inputPath = './src/documentos/result.docx';
-      const outputPath = './src/documentos/result.pdf';
+        const ext = '.pdf';
+        const inputPath = './src/documentos/result.docx';
+        const outputPath = './src/documentos/result.pdf';
 
-      const docxBuf = await fs.promises.readFile(inputPath);
+        const docxBuf = await fs.promises.readFile(inputPath);
 
-      const pdfBuf = (await libreAsyncConvert(
-        docxBuf,
-        ext,
-        undefined,
-      )) as Buffer;
-      await fs.promises.writeFile(outputPath, pdfBuf);
+        const pdfBuf = (await libreAsyncConvert(
+          docxBuf,
+          ext,
+          undefined,
+        )) as Buffer;
+        await fs.promises.writeFile(outputPath, pdfBuf);
 
-      const pdfAsBuffer = await fs.promises.readFile(outputPath);
+        const pdfAsBuffer = await fs.promises.readFile(outputPath);
 
-      const pdfDoc = await PDFDocument.load(pdfAsBuffer, {
-        ignoreEncryption: true,
-      });
+        const pdfDoc = await PDFDocument.load(pdfAsBuffer, {
+          ignoreEncryption: true,
+        });
 
-      const urlCv = data.name;
+        const urlCv = data.name;
 
-      const QR = await qrcode.toDataURL(urlCv);
+        const QR = await qrcode.toDataURL(urlCv);
 
-      const Base64QR = QR.replace(/^data:image\/png;base64,/, '');
+        const Base64QR = QR.replace(/^data:image\/png;base64,/, '');
 
-      fs.writeFileSync('./src/documentos/QR.png', Base64QR, 'base64');
+        fs.writeFileSync('./src/documentos/QR.png', Base64QR, 'base64');
 
-      const imageBuffer = await fs.promises.readFile('./src/documentos/QR.png');
+        const imageBuffer = await fs.promises.readFile(
+          './src/documentos/QR.png',
+        );
 
-      const pngImage = await pdfDoc.embedPng(imageBuffer);
-      const pages = pdfDoc.getPages();
-      const pngDims = pngImage.scale(1);
-      pages[0].drawImage(pngImage, {
-        x: 235,
-        y: 205,
-        width: pngDims.width,
-        height: pngDims.height,
-      });
+        const pngImage = await pdfDoc.embedPng(imageBuffer);
+        const pages = pdfDoc.getPages();
+        const pngDims = pngImage.scale(0.6);
+        pages[0].drawImage(pngImage, {
+          x: 70,
+          y: 192,
+          width: pngDims.width,
+          height: pngDims.height,
+        });
 
-      await fs.promises.writeFile(
-        './src/documentos/withQR.pdf',
-        await pdfDoc.save({ useObjectStreams: false }),
-      );
+        await fs.promises.writeFile(
+          './src/documentos/withQR.pdf',
+          await pdfDoc.save({ useObjectStreams: false }),
+        );
 
-      const pdfBuffer = fs.readFileSync('./src/documentos/withQR.pdf');
+        const pdfBuffer = fs.readFileSync('./src/documentos/withQR.pdf');
 
-      res.setHeader('Content-Type', 'application/pdf');
-      res.send(pdfBuffer);
+        res.setHeader('Content-Type', 'application/pdf');
+        res.send(pdfBuffer);
 
-      fs.unlinkSync(outputPath);
-      fs.unlinkSync('./src/documentos/withQR.pdf');
-      fs.unlinkSync('./src/documentos/QR.png');
-      fs.unlinkSync(inputPath);
-    });
+        fs.unlinkSync(outputPath);
+        fs.unlinkSync('./src/documentos/withQR.pdf');
+        fs.unlinkSync('./src/documentos/QR.png');
+        fs.unlinkSync(inputPath);
+      },
+    );
     return 'Carbone exitoso';
   }
 
